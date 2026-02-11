ffad4fb067d2315d014bc7c9e31cd65d44afe2d1
vitorhassis <vitorassis997@gmail.com>
2026-02-11 00:51:35 -0300

    Add gitignore

--- Cashflowr/Cashflowr/obj/Debug/net6.0/Cashflowr.AssemblyInfo.cs
+++ Cashflowr/Cashflowr/obj/Debug/net6.0/Cashflowr.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cashflowr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecfaf44338581e5ac9987488321f182722b136db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7f9357599ff5a99e365cc43e8fb8991211534ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cashflowr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cashflowr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
